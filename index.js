aaba0e0917a7e8baf8117d7e9c8303da98afd254
Vanessa <114338599+Moneygame34@users.noreply.github.com>
2023-01-24 20:39:38 -0700

    Add files via upload
    W2 Assignment External JavaScript

---
+++ index.js
@@ -0,0 +1,8 @@
+/*This is the index javascript for the file index file html */
+/*Started and Completed by Vanessa Bonner */
+/*Date: January 22, 2023 */
+
+function Linkwindow(){
+    window.open("https://www.google.com");
+    
+}
